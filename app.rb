2baa7046219090818097b639130a900b24dd0a07
tomoaki-kimura <tkim19750404@gmail.com>
2019-07-30 07:52:18 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+	p params
+  # ここにコードを入力してください。
+  @name = params[:name]
+  @birth = params[:birth]
+  @food = params[:food]
+  # ここまでコードを入力してね。
+  erb :index
+end
+
+# Lesson9変数を使ってプログラミング
